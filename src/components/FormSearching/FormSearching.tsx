ac495d3e9c7bb040b33984552917589863023602
Maxim P <raiyin@ya.ru>
2023-01-27 20:19:20 +0300

    Form hiding added.

--- src/components/FormSearching/FormSearching.tsx
+++ src/components/FormSearching/FormSearching.tsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
-import MyButton from '../ui/MyButton/MyButton';
-import MyInput from '../ui/MyInput/MyInput';
-import cl from './FormSearching.module.css';
+import React, { useState } from 'react'
+import MyButton from '../ui/MyButton/MyButton'
+import MyInput from '../ui/MyInput/MyInput'
+import cl from './FormSearching.module.css'
 
 export default function FormSearching() {
-
-    const [btnText, setBtnText] = useState('Показать настройки');
-    const [showOrHideSettings, setShowOrHideSettings] = useState(true);
+    const [btnText, setBtnText] = useState('Показать настройки')
+    const [showOrHideSettings, setShowOrHideSettings] = useState(false)
 
     function changeButtonText() {
-        setBtnText(showOrHideSettings ? 'Показать настройки' : 'Скрыть настройки');
+        setBtnText(showOrHideSettings ? 'Показать настройки' : 'Скрыть настройки')
     }
 
     const changeVision = () => {
-        setShowOrHideSettings(!showOrHideSettings);
-        changeButtonText();
-    };
+        setShowOrHideSettings(!showOrHideSettings)
+        changeButtonText()
+    }
 
     return (
         <div className={cl.formsearching}>
             <MyButton onClick={changeVision} text={btnText} />
-            <MyInput placeholder='Login' />
-            <MyInput placeholder='Repository name' />
+            {showOrHideSettings ? (
+                <>
+                    <MyInput placeholder="Login" />
+                    <MyInput placeholder="Repository name" />
+                </>
+            ) : (
+                <></>
+            )}
         </div>
-    );
+    )
 }
