facc260c66eec67f2a80766313600bc412d5d81f
Maxim P <raiyin@ya.ru>
2023-02-09 22:49:10 +0300

    Protection to localstorage api was added.

--- src/api/localstorageService.ts
+++ src/api/localstorageService.ts
@@ -4,79 +4,129 @@ let repoNameKey = 'repoName';
 let reviewerKey = 'reviewer';
 
 let localStorage = window.localStorage;
-// add try/catch
 
 export function addUserToBlackList(login: string, avatar_url: string) {
-
-    let blackListString = localStorage.getItem(blackListKey);
-    if (blackListString !== null) {
-        let blackList: Array<{ login: string, avatar_url: string; }> = JSON.parse(blackListString);
-        blackList.push({ 'login': login, 'avatar_url': avatar_url });
-        blackListString = JSON.stringify(blackList);
-        localStorage.setItem(blackListKey, blackListString);
-    }
-    else {
-        let blackList: Array<{ login: string, avatar_url: string; }> = [];
-        blackList.push({ login: login, avatar_url: avatar_url });
-        blackListString = JSON.stringify(blackList);
-        localStorage.setItem(blackListKey, blackListString);
+    try {
+        let blackListString = localStorage.getItem(blackListKey);
+        if (blackListString !== null) {
+            let blackList: Array<{ login: string, avatar_url: string; }> = JSON.parse(blackListString);
+            blackList.push({ 'login': login, 'avatar_url': avatar_url });
+            blackListString = JSON.stringify(blackList);
+            localStorage.setItem(blackListKey, blackListString);
+        }
+        else {
+            let blackList: Array<{ login: string, avatar_url: string; }> = [];
+            blackList.push({ login: login, avatar_url: avatar_url });
+            blackListString = JSON.stringify(blackList);
+            localStorage.setItem(blackListKey, blackListString);
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
 export function removeUserFromBlackList(login: string) {
-    let blackListString = localStorage.getItem(blackListKey);
-    if (blackListString !== null) {
-        let blackList: Array<{ login: string, avatar_url: string; }> = JSON.parse(blackListString);
-        blackList = blackList.filter(item => item.login !== login);
-        blackListString = JSON.stringify(blackList);
-        localStorage.setItem(blackListKey, blackListString);
+    try {
+        let blackListString = localStorage.getItem(blackListKey);
+        if (blackListString !== null) {
+            let blackList: Array<{ login: string, avatar_url: string; }> = JSON.parse(blackListString);
+            blackList = blackList.filter(item => item.login !== login);
+            blackListString = JSON.stringify(blackList);
+            localStorage.setItem(blackListKey, blackListString);
+        }
+    }
+    catch (error) {
+        console.log(error);
     }
 }
 
 export function getAllUsersFromBlackList() {
-
-    let blackListString = localStorage.getItem(blackListKey);
     let blackList: Array<{ login: string, avatar_url: string; }> = [];
-    if (blackListString !== null) {
-        blackList = JSON.parse(blackListString);
+    try {
+        let blackListString = localStorage.getItem(blackListKey);
+        if (blackListString !== null) {
+            blackList = JSON.parse(blackListString);
+        }
     }
+    catch (error) { console.log(error); }
     return blackList;
 }
 
 export function getMainUser() {
-    return localStorage.getItem(mainUserKey);
+    try {
+        return localStorage.getItem(mainUserKey);
+    } catch (error) {
+        console.log(error);
+        return '';
+    }
 }
 
 export function setMainUser(login: string) {
-    localStorage.setItem(mainUserKey, login);
+    try {
+        localStorage.setItem(mainUserKey, login);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function unsetMainUser() {
-    localStorage.removeItem(mainUserKey);
+    try {
+        localStorage.removeItem(mainUserKey);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
 export function getRepo() {
-    return localStorage.getItem(repoNameKey);
+    try {
+        return localStorage.getItem(repoNameKey);
+    } catch (error) {
+        console.log(error);
+        return '';
+    }
 }
 
 export function setRepo(repo: string) {
-    localStorage.setItem(repoNameKey, repo);
+    try {
+        localStorage.setItem(repoNameKey, repo);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function unsetRepo() {
-    localStorage.removeItem(repoNameKey);
+    try {
+        localStorage.removeItem(repoNameKey);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function getReviewer(): { login: string, avatar_url: string; } | null {
-    let temp = localStorage.getItem(reviewerKey);
-    if (temp !== null) { return JSON.parse(temp); }
-    else return null;
+    try {
+        let temp = localStorage.getItem(reviewerKey);
+        if (temp !== null) { return JSON.parse(temp); }
+        else return null;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
 
 export function setReviewer(login: string, avatar_url: string) {
-    localStorage.setItem(reviewerKey, JSON.stringify({ 'login': login, 'avatar_url': avatar_url }));
+    try {
+        localStorage.setItem(reviewerKey, JSON.stringify({ 'login': login, 'avatar_url': avatar_url }));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function removeReviewer() {
-    localStorage.removeItem(reviewerKey);
+    try {
+        localStorage.removeItem(reviewerKey);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
